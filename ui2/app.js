9142e5ded2eb3ec6c9f00338e4593eefc7ab9d30
Robert Wagner <rw@20sec.net>
2024-09-03 20:55:13 +0200

    ui more simple

--- ui2/app.js
+++ ui2/app.js
@@ -1,70 +1,36 @@
 import api from "./api.js";
 import NavigoPlus from "./components/navigo-plus.js";
 let main = document.querySelector("main");
-let spinner = document.getElementById("spinner");
-let time_spent = document.getElementById("time-spent");
 let refetch_btn = document.querySelector("#refetch");
 
 /*
   routing
 */
-
 const router = NavigoPlus("/__ui2");
 router
   .on("/type/:type", ({ data }) => {
-    console.log("navigated to type", data); // { id: 'xxx', action: 'save' }
-    page_type(data.type);
+    main.innerHTML = `<types-index type="${data.type}"></types-index>`;
   })
   .on("/id/:id", ({ data }) => {
-    console.log("navigated to id", data); // { id: 'xxx', action: 'save' }
-    page_id(data.id);
+    main.innerHTML = `<document-view docid="${data.id}"></document-view>`;
   })
   .on("/about", () => {
-    page_about();
+    main.innerHTML = `<h1>slowfoot navigator</h1>
+    <p>navigate through your data</p>`;
   })
   .on("/", () => {
-    page_index();
+    main.innerHTML = ``;
   });
 
-// console.log("++ router", router);
 /*
-  global fetch-status
+  refetch action
 */
-document.addEventListener("fetch-start", () =>
-  spinner.removeAttribute("hidden")
-);
-document.addEventListener("fetch-end", (e) => {
-  spinner.setAttribute("hidden", "");
-  time_spent.innerHTML = `<span>${e.detail?.time_print}</span>`;
-  // router.updatePageLinks();
-});
 refetch_btn.addEventListener("click", () => {
   refetch_btn.disabled = true;
   api.refetch().finally(() => (refetch_btn.disabled = false));
 });
-/*
-render pages
-*/
-
-function page_type(type) {
-  main.innerHTML = `<types-index type="${type}"></types-index>`;
-}
-
-function page_id(id) {
-  main.innerHTML = `<document-view docid="${id}"></document-view>`;
-}
-
-function page_about() {
-  main.innerHTML = `<h1>slowfoot navigator</h1>
-    <p>navigate through your data</p>`;
-}
-
-function page_index() {
-  main.innerHTML = ``;
-}
 
 /*
-  initiale action
+  initial action
 */
 router.start_from_current_url();
-/* -- */
