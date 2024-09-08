12e735808c181ca64a0bef716e9a2916278e2f61
rw <rw@20sec.net>
2024-09-09 01:03:51 +0200

    runner

--- Makefile
+++ Makefile
@@ -12,3 +12,8 @@ analyze:
 min-ui:
 	rollup -f es ui/app.js > ui/app-bundle.js
 	terser ui/app-bundle.js -o ui/app-bundle.min.js
+
+runner:
+	# tail -n +4 -q vendor/vlucas/phpdotenv/src/**/*.php > dot.php
+	# sed -i -e 's/declare/#declare/g' dot.php
+	tail -n +2 -q src/functions/**/*.php > runner.php
--- src/functions/request.php
+++ src/functions/request.php
@@ -7,13 +7,15 @@ class request {
     public $method;
     public $get;
     public $post;
+    public $env = [];
     public array $body = [];
 
-    public function __construct($url) {
+    public function __construct($url, $env = []) {
         $this->name = $url;
         $this->method = strtolower($_SERVER["REQUEST_METHOD"]);
         $this->get = $_GET;
         $this->post = $_POST;
+        $this->env = $env;
         $body = file_get_contents('php://input');
         if ($body) $this->body = json_decode($body, true);
     }
