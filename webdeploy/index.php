51906f8d004a5abc67a6e70bd65152a5c7bbe7c6
Robert Wagner <rw@20sec.net>
2024-10-04 15:08:19 +0200

    webdeploy

--- webdeploy/index.php
+++ webdeploy/index.php
@@ -1,4 +1,9 @@
 <?php
+/*
+apache:
+SetEnv SLFT_BUILD_KEY ycx-sdfsdf-sdf213213-ewrwe-dfs
+Alias /__web-deploy /var/www/vhosts/kurparkverlag/slowfoot/web-deploy/index.php
+*/
 
 define('SLOWFOOT_START', microtime(true));
 define('SLOWFOOT_BASE', dirname(__DIR__));
