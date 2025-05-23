54277a21e965b06a8cb747949a2eb7f5e12c294f
Robert Wagner <rw@20sec.net>
2025-05-23 20:17:42 +0200

    exif extension

--- resources/projects/minimal/src/pages/index.phue.php
+++ resources/projects/minimal/src/pages/index.phue.php
@@ -4,7 +4,7 @@
 
     <p>to the world</p>
 
-    <sf.image_tag src="src/pages/kitty.jpg" size="600x" alt="this is the cat"></sf.image_tag>
+    <sft.image src="src/pages/kitty.jpg" size="600x" alt="this is the cat"></sft.image>
 
     <caption>Foto von <a href="https://unsplash.com/de/@yerlinmatu?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Yerlin Matu</a>
       auf <a href="https://unsplash.com/de/fotos/flachfokusfotografie-von-weissen-und-braunen-katzen-GtwiBmtJvaU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
