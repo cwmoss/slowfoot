3bdadae085f863e255a2dbce277fb4e44041afe6
Robert Wagner <rw@20sec.net>
2026-02-18 18:42:25 +0100

    databucket helper

--- src/databucket.php
+++ src/databucket.php
@@ -19,6 +19,34 @@ class databucket implements ArrayAccess {
         return new self($d);
     }
 
+    public function get_path(array|string $path, $default = null) {
+        if (!is_array($path)) $path = explode(".", $path);
+        $current = $this->data;
+        foreach ($path as $key) {
+            if (is_array($current)) {
+                if (!isset($current[$key])) {
+                    $current = $default;
+                    break;
+                } else {
+                    $current = $current[$key];
+                }
+            } elseif (is_object($current)) {
+                if (!isset($current->$key)) {
+                    $current = $default;
+                    break;
+                } else {
+                    $current = $current->$key;
+                }
+            } else {
+                $current = $default;
+                break;
+            }
+        }
+        if (is_null($current)) return null;
+        if (is_scalar($current)) return $current;
+        return new self($current);
+        // return $current;
+    }
     public function offsetSet($offset, $value): void {
     }
 
@@ -72,6 +100,14 @@ class databucket implements ArrayAccess {
 // https://www.php.net/manual/en/class.arrayobject.php
 
 /*
+class image {
+
+    public function __construct(public int $w, public int $h) {
+    }
+    public function size() {
+        return sprintf("%sx%s", $this->w, $this->h);
+    }
+}
 
 $test = [
     'name' => 'David Hayes',
@@ -85,19 +121,39 @@ $test = [
     "mother" => ["name" => "maria"]
 ];
 
-$d = new databucket($test)->with("title", "a person");
+$d = new databucket($test)->with("title", "a person")->with("img", new image(300, 200));
 
-echo $d->mother->name, $d->to_buy->{2}, $d->title, "\n";
-echo $d['mother']['name'], $d['to_buy'][2], $d["title"], "\n";
+echo $d->mother->name, $d->to_buy->{2}, $d->title, $d->img->w, "\n";
+echo $d['mother']['name'], $d['to_buy'][2], $d["title"], $d["img"]["w"], "\n";
 
 var_dump($d['to_buy']);
+
+echo $d->img->unwrap()->size();
+echo $d['img']->unwrap()->size();
 
 $test = databucket::array_to_object($test);
 
-$d = new databucket($test)->with("title", "a person");
+$d = new databucket($test)->with("title", "a person")->with("img", new image(300, 200));
 
-echo $d->mother->name, $d->to_buy->{2}, $d->title, "\n";
-echo $d['mother']['name'], $d['to_buy'][2], $d["title"], "\n";
+echo $d->mother->name, $d->to_buy->{2}, $d->title, $d->img->w, "\n";
+echo $d['mother']['name'], $d['to_buy'][2], $d["title"], $d["img"]["w"], "\n";
 
 var_dump($d['to_buy']);
+
+
+echo $d->img->unwrap()->size(), "\n";
+echo $d['img']->unwrap()->size(), "\n";
+
+$d = new databucket(new image(300, 200));
+echo $d->unwrap()->size(), "\n";
+echo $d["w"], "\n";
+echo $d->h, "\n";
+
+$d = new databucket($test)->with("title", "a person")->with("img", new image(300, 200));
+
+echo $d->get_path("mother.0.name"), "\n";
+echo $d->get_path("mother.name"), "\n";
+echo $d->get_path("to_buy.0"), "\n";
+echo $d->get_path("to_buy.0.price"), "\n";
+print_r($d->get_path("mother"));
 */
