b8620e1c58c0ad5a5e9b78e0ec3a43fce7f37e27
rw <rw@20sec.net>
2026-04-01 14:43:39 +0200

    make phar

--- gen_phar.php
+++ gen_phar.php
@@ -4,6 +4,8 @@
 // https://gnugat.github.io/2026/03/25/turn-you-php-app-into-a-standalone-binary.html
 // 
 // better set to php.ini phar.readonly = 0
+// https://stackoverflow.com/questions/20264737/php-list-directory-structure-and-exclude-some-directories
+
 ini_set("phar.readonly", 0);
 
 $pharFile = 'app.phar';
@@ -39,3 +41,39 @@ $p->setDefaultStub('php/bin/api.php');
 $p->compress(Phar::GZ);
 
 echo "$pharFile successfully created";
+
+/*
+
+// Source - https://stackoverflow.com/a/67459883
+// Posted by Miloš Đakonović
+// Retrieved 2026-04-01, License - CC BY-SA 4.0
+
+$exclude = ['file0.txt', 'file1.txt', 'file2.txt'];
+*/
+/**
+ * @param SplFileInfo $file
+ * @param mixed $key
+ * @param RecursiveCallbackFilterIterator $iterator
+ * @return bool True if you need to recurse or if the item is acceptable
+ */
+/*
+$filter = function ($file, $key, $iterator) use ($exclude) {
+    foreach($exclude as $excludefilename){
+        if(
+            strcmp(__DIR__ . DIRECTORY_SEPARATOR . $excludefilename, $file) === 0
+        ) return false;
+    }
+    return true;
+};
+
+$innerIterator = new RecursiveDirectoryIterator(
+    __DIR__,
+    RecursiveDirectoryIterator::SKIP_DOTS
+);
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveCallbackFilterIterator($innerIterator, $filter)
+);
+
+$phar = new PharData('project.tar');
+$phar->buildFromIterator($iterator, __DIR__);
+*/
